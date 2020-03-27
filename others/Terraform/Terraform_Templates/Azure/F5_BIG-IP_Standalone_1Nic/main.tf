6faecece30974b8af2daf6fbc32579a1463f795d
MattDierick <matt262810@gmail.com>
2020-03-27 19:31:34 +0100

    features

--- others/Terraform/Terraform_Templates/Azure/F5_BIG-IP_Standalone_1Nic/main.tf
+++ others/Terraform/Terraform_Templates/Azure/F5_BIG-IP_Standalone_1Nic/main.tf
@@ -9,6 +9,10 @@
 ##    tenant_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 ##}
 provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+  # If you are using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
 }
 
 data "azurerm_resource_group" "rg_keyvault" {
