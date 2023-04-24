64967ee1ede484df9d70fd10b6d1f1da7ce9ec16
MattDierick <matt262810@gmail.com>
2023-04-24 14:57:54 +0200

    api-auth

--- scripts/Traffic_generator/full-tc.sh
+++ scripts/Traffic_generator/full-tc.sh
@@ -3,4 +3,5 @@ sh tc.sh arcadia-re.emea-ent.f5demos.com 50;
 sh tc.sh arcadia-re-ce.emea-ent.f5demos.com 50;
 sh tc.sh sentence.emea-ent.f5demos.com 50;
 sh tc.sh api-sentence.emea-ent.f5demos.com 50;
+sh tc-http.sh azure-f5xc-ce.westeurope.cloudapp.azure.com 50;
 done
