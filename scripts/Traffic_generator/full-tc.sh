7797c9cd0310862f09a53b56f3ae2fb1736a24c8
Matt <matt262810@gmail.com>
2025-02-07 14:39:29 +0100

    update tc-full

--- scripts/Traffic_generator/full-tc.sh
+++ scripts/Traffic_generator/full-tc.sh
@@ -1,7 +1,9 @@
-for ((i=1; i<=$1; i++)); do
-sh tc.sh arcadia-re.emea-ent.f5demos.com 50;
-sh tc.sh arcadia-re-ce.emea-ent.f5demos.com 50;
-sh tc.sh sentence.emea-ent.f5demos.com 50;
-sh tc.sh api-sentence.emea-ent.f5demos.com 50;
-sh tc-http.sh azure-f5xc-ce.westeurope.cloudapp.azure.com 50;
+#!/bin/sh
+for ((j=1; j<=5; j++)); do
+bash tc.sh arcadia-re.emea-ent.f5demos.com 50;
+bash api.sh api-sentence.emea-ent.f5demos.com 50;
+bash tc.sh arcadia-re-ce.emea-ent.f5demos.com 50;
+bash tc.sh api-sentence.emea-ent.f5demos.com 50;
+bash tc-http.sh azure-f5xc-ce.westeurope.cloudapp.azure.com 50;
+bash api-auth.sh api-sentence-auth.emea-ent.f5demos.com 50;
 done
